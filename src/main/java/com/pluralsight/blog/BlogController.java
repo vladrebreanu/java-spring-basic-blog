8ab9106508fef8eed1dcd4ba648aa9fc3d2873c9
Vlad Rebreanu <vlad.rebreanu@gmail.com>
2021-01-28 17:21:57 +0200

    Added home
    msg

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,17 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+
+    @RequestMapping("/")
+    public String listPosts(ModelMap modelMap) {
+
+        modelMap.put("title", "Blog post 1");
+        return "home";
+    }
 
 }
